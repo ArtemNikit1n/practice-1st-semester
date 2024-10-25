eb206bb56a164b23976da1a4c42cfcfbc315eba0
psiblvdegod <artyombaly@list.ru>
2024-10-25 16:17:25 +0300

    Update queue.c

--- Queue/Queue/queue.c
+++ Queue/Queue/queue.c
@@ -1,33 +1,33 @@
-﻿﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
 #include "queue.h"
 
 typedef struct QueueElement {
     int value;
-    struct QueueElement* next;
+    struct QueueElement* previous;
 } QueueElement;
 
 struct Queue {
-    QueueElement* front;
-    QueueElement* back;
+    QueueElement * front;
+    QueueElement * back;
 };
 
 void Dequeue(Queue* queue) {
     QueueElement* tmp = queue->front;
-    queue->front = queue->front->next;
+    queue->front = queue->front->previous;
     free(tmp);
 }
 
 /*Stack* createStack() {
     return (Stack*)calloc(1, sizeof(Stack));
-}*/
+*/
 
-void Enqueue(Queue* stack, int value) {
+void Enqueue(Queue* queue, int value) {
     QueueElement* element = malloc(sizeof(QueueElement));
     assert(element != NULL);
+    element->previous = NULL;
     element->value = value;
-    element->next = stack->front;
-    stack->front = element;
+    queue->back->previous = element;
+    queue->back = element;
 }
