27724ce78e5b3db3b98812d29047f5359f25ff18
psiblvdegod <artyombaly@list.ru>
2024-10-25 16:50:54 +0300

    Update queue.h

--- Queue/Queue/queue.h
+++ Queue/Queue/queue.h
@@ -1,9 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
 #pragma once
-
 typedef struct Queue Queue;
-
 void enqueue(Queue * queue, int value);
-
 void dequeue(Queue* queue);
-
 Queue* createQueue();
